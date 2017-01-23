a575799b5a7ddbbffab0d992e51c44796885371f
Sean P. McDonald <sean.mcdonald@puppetlabs.com>
2017-01-23 09:53:11 -0800

    (RE-6628) Update nightly_repo job with new pack_signed_repo tasks
    Our automation is going to need to handle more that one set of platforms to
    promote to PE. Instead of adding seperate jobs for seperate pipelines (i.e.
    one for master, one for stable etc.) and continuing to carry around static
    lists on top of information we already have, this commit adds the ability to
    use the platform information in build_defaults.yaml to identify what platforms
    promote.
    
    This is done through a new pack_all_signed_repos_indivudually task, to replace
    the pack_signed_repo task (that created the need for the list in the jenkins
    job).
    
    to faciliate this, the platform_repos data has been added to the available
    data loaded in build_defaults.yaml, this data is in the following form:
    
    platform_repos:
      - name: platform-version-arch
        repo_location: repos/platform/repo/location

---
+++ lib/packaging/repo.rb
@@ -0,0 +1,37 @@
+module Pkg::Repo
+
+  class << self
+    def create_signed_repo_archive(path_to_repo, name_of_archive, versioning)
+      tar = Pkg::Util::Tool.check_tool('tar')
+      puts "DEBUG: #{name_of_archive}"
+      puts "DEBUG: #{path_to_repo}"
+      Dir.chdir("pkg") do
+        if versioning == 'ref'
+          local_target = File.join(Pkg::Config.project, Pkg::Config.ref)
+        elsif versioning == 'version'
+          local_target = File.join(Pkg::Config.project, Pkg::Util::Version.get_dot_version)
+        end
+        Dir.chdir(local_target) do
+          if Pkg::Util::File.empty_dir?(path_to_repo)
+            if ENV['FAIL_ON_MISSING_TARGET'] == "true"
+              raise "ERROR: missing packages under #{path_to_repo}"
+            else
+              warn "Skipping #{name_of_archive} because #{path_to_repo} has no files"
+            end
+          else
+            stdout, _, _ = Pkg::Util::Execution.capture3("#{tar} --owner=0 --group=0 --create --gzip --file #{File.join('repos', "#{name_of_archive}.tar.gz")} #{path_to_repo}")
+            stdout
+          end
+        end
+      end
+    end
+
+    def create_all_repo_archives(project, versioning)
+      platforms = Pkg::Config.platform_repos
+      platforms.each do |platform|
+        archive_name = "#{project}-#{platform['name']}"
+        create_signed_repo_archive(platform['repo_location'], archive_name, versioning)
+      end
+    end
+  end
+end
